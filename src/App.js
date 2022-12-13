6a5ae277a3da2eed4a667e165fb894856c366ba7
Vipin Negi <vipin@mindrops.com>
2022-12-13 23:12:53 +0530

    Defining & Using Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
       <h2>Let's get started!</h2>
     </div>
   );
---
+++ src/pages/Welcome.js
@@ -0,0 +1,7 @@
+const Welcome = () => {
+  return <h1>The Welcome Page</h1>;
+};
+
+export default Welcome;
+
+//since welcome is a screen, we have putted this inside pages folder and not the component folder
